f50992576a79852652e5e8574a83d45f1d147262
invincible0110 <sson1235648@naver.com>
2023-01-13 21:46:36 +0900

    터레인 설정

--- Unity3d/second/Assets/FirstClass/Script/Test.cs
+++ Unity3d/second/Assets/FirstClass/Script/Test.cs
@@ -12,16 +12,47 @@ class Monster
 public class Test : MonoBehaviour
 {
     public int value = 100;
+    public int[] count;
+    public int[] data = new int[3];
 
     void Start()
     {
         Debug.Log(value);
         Monster monster = new Monster();
+
+        transform.position = new Vector3(0, transform.position.y, 1);
+
+        foreach(var element in data)
+        {
+            Debug.Log(element);
+        }
     }
 
     
     void Update()
     {
-        
+        // GetKeyDown : 키를 눌렀을 때
+        // GetKey : 키를 누르고 있을 때
+        // GetKeyUp : 키를 뗐을 때
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("스페이스 누름");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x-1, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x+1, transform.position.y, transform.position.z);
+        }
     }
 }

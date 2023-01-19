a6c0efae0a676da6bfa43f8f014e4e6e0fcf3a8c
invincible0110 <sson1235648@naver.com>
2023-01-19 21:51:31 +0900

    Animation Blend

---
+++ Unity3d/second/Assets/Animation Blend/Script/Motion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxis = -1 ~ 0 ~ 1 사이의 값을 반환합니다.
+        float vertical = Input.GetAxis("Vertical"); // 키입력 감지
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Horizontal", horizontal);
+    }
+}

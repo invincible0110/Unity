92719731c0d3e50fd11688e018a3fc8e676332d3
invincible0110 <sson1235648@naver.com>
2023-01-31 21:47:17 +0900

    Light

---
+++ Unity3d/second/Assets/Light/Script/LightManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    bool[] condition = new bool[3];
+    public GameObject[] lightEffect;
+
+    private void Start()
+    {
+        for(int i = 0; i < 3; i++) 
+        {
+            condition[i] = false;
+        }
+    }
+
+    public void LightSetting(int number)
+    {
+        condition[number] = !condition[number];
+
+        lightEffect[number].SetActive(condition[number]); 
+    }
+
+    
+}

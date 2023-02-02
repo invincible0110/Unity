d581a9eb532b65c2ec7091c3ee416e0ad7505023
invincible0110 <sson1235648@naver.com>
2023-02-02 21:47:54 +0900

    Animation Retageting

--- Unity3d/second/Assets/Nev Mash Obstacle/Script/AutoMove.cs
+++ Unity3d/second/Assets/Nev Mash Obstacle/Script/AutoMove.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class AutoMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
+   
     void Update()
     {
-        
+        transform.position = new Vector3
+        (
+            Mathf.PingPong(Time.time, 3),
+            transform.position.y,
+                transform.position.z
+        );
     }
 }
